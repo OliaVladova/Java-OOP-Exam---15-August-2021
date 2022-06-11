b21cc025f25be9c375f6d1a985aeac962202133c
OliaVladova <ovladova1@gmail.com>
2022-06-11 23:28:39 +0300

    Add files via upload

---
+++ restaurant/core/ControllerImpl.java
@@ -0,0 +1,149 @@
+package restaurant.core;
+
+import restaurant.common.ExceptionMessages;
+import restaurant.common.OutputMessages;
+import restaurant.core.interfaces.Controller;
+import restaurant.entities.drinks.Fresh;
+import restaurant.entities.drinks.Smoothie;
+import restaurant.entities.healthyFoods.Salad;
+import restaurant.entities.healthyFoods.VeganBiscuits;
+import restaurant.entities.healthyFoods.interfaces.HealthyFood;
+import restaurant.entities.drinks.interfaces.Beverages;
+import restaurant.entities.tables.InGarden;
+import restaurant.entities.tables.Indoors;
+import restaurant.entities.tables.interfaces.Table;
+import restaurant.repositories.interfaces.*;
+
+public class ControllerImpl implements Controller {
+    private HealthFoodRepository<HealthyFood> healthFoodRepository;
+    private BeverageRepository<Beverages> beverageRepository;
+    private TableRepository<Table> tableRepository;
+    private double totalMoney;
+
+    public ControllerImpl(HealthFoodRepository<HealthyFood> healthFoodRepository,
+                          BeverageRepository<Beverages> beverageRepository,
+                          TableRepository<Table> tableRepository) {
+
+        this.healthFoodRepository = healthFoodRepository;
+        this.beverageRepository = beverageRepository;
+        this.tableRepository = tableRepository;
+    }
+
+    @Override
+    public String addHealthyFood(String type, double price, String name) {
+        HealthyFood food = type.equals("Salad")
+                ? new Salad(name, price)
+                : new VeganBiscuits(name, price);
+
+        HealthyFood previouslyAdded = this.healthFoodRepository.foodByName(name);
+
+        if (previouslyAdded != null) {
+            throw new IllegalArgumentException(String.format(ExceptionMessages.FOOD_EXIST, name));
+        }
+
+        this.healthFoodRepository.add(food);
+        return String.format(OutputMessages.FOOD_ADDED, name);
+    }
+
+    @Override
+    public String addBeverage(String type, int counter, String brand, String name) {
+        Beverages beverage = type.equals("Fresh")
+                ? new Fresh(name, counter, brand)
+                : new Smoothie(name, counter, brand);
+
+        Beverages previouslyAdded = this.beverageRepository.beverageByName(name, brand);
+
+        if (previouslyAdded != null) {
+            throw new IllegalArgumentException(String.format(ExceptionMessages.BEVERAGE_EXIST, name));
+        }
+
+        this.beverageRepository.add(beverage);
+        return String.format(OutputMessages.BEVERAGE_ADDED, type, brand);
+    }
+
+    @Override
+    public String addTable(String type, int tableNumber, int capacity) {
+        Table table = type.equals("Indoors")
+                ? new Indoors(tableNumber, capacity)
+                : new InGarden(tableNumber, capacity);
+
+        Table previouslyAdded = this.tableRepository.byNumber(tableNumber);
+
+        if (previouslyAdded != null) {
+            throw new IllegalArgumentException(String.format(ExceptionMessages.TABLE_IS_ALREADY_ADDED, tableNumber));
+        }
+
+        this.tableRepository.add(table);
+        return String.format(OutputMessages.TABLE_ADDED, tableNumber);
+    }
+
+    @Override
+    public String reserve(int numberOfPeople) {
+        Table suitableTable = this.tableRepository
+                .getAllEntities()
+                .stream()
+                .filter(table -> !table.isReservedTable() &&
+                        table.getSize() >= numberOfPeople)
+                .findFirst()
+                .orElse(null);
+
+        if (suitableTable == null) {
+            return String.format(OutputMessages.RESERVATION_NOT_POSSIBLE, numberOfPeople);
+        }
+
+        suitableTable.reserve(numberOfPeople);
+        return String.format(OutputMessages.TABLE_RESERVED, suitableTable.getTableNumber(), numberOfPeople);
+    }
+
+    @Override
+    public String orderHealthyFood(int tableNumber, String healthyFoodName) {
+        Table table = this.tableRepository.byNumber(tableNumber);
+
+        if (table == null) {
+            return String.format(OutputMessages.WRONG_TABLE_NUMBER, tableNumber);
+        }
+
+        HealthyFood healthyFood = this.healthFoodRepository.foodByName(healthyFoodName);
+
+        if (healthyFood == null) {
+            return String.format(OutputMessages.NONE_EXISTENT_FOOD, healthyFoodName);
+        }
+
+        table.orderHealthy(healthyFood);
+        return String.format(OutputMessages.FOOD_ORDER_SUCCESSFUL, healthyFoodName, tableNumber);
+    }
+
+    @Override
+    public String orderBeverage(int tableNumber, String name, String brand) {
+        Table table = this.tableRepository.byNumber(tableNumber);
+
+        if (table == null) {
+            return String.format(OutputMessages.WRONG_TABLE_NUMBER, tableNumber);
+        }
+
+        Beverages beverages = this.beverageRepository.beverageByName(name, brand);
+
+        if (beverages == null) {
+            return String.format(OutputMessages.NON_EXISTENT_DRINK, name, brand);
+        }
+
+        table.orderBeverages(beverages);
+        return String.format(OutputMessages.BEVERAGE_ORDER_SUCCESSFUL, name, tableNumber);
+    }
+
+    @Override
+    public String closedBill(int tableNumber) {
+        Table table = this.tableRepository.byNumber(tableNumber);
+
+        double tableBill = table.bill();
+        table.clear();
+        totalMoney += tableBill;
+
+        return String.format(OutputMessages.BILL, tableNumber, tableBill);
+    }
+
+
+    @Override
+    public String totalMoney() {
+        return String.format(OutputMessages.TOTAL_MONEY, totalMoney);
+    }}
