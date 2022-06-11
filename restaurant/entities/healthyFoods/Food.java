22979a7a4592ecad29fecaed382beab4c9aa2703
OliaVladova <ovladova1@gmail.com>
2022-06-11 23:43:28 +0300

    Add files via upload

---
+++ restaurant/entities/healthyFoods/Food.java
@@ -0,0 +1,55 @@
+package restaurant.entities.healthyFoods;
+
+import restaurant.common.ExceptionMessages;
+import restaurant.entities.healthyFoods.interfaces.HealthyFood;
+
+public abstract class Food implements HealthyFood {
+    private String name;
+    private double portion;
+    private double price;
+
+    protected Food(String name, double portion, double price) {
+        this.setName(name);
+        this.setPortion(portion);
+        this.setPrice(price);
+    }
+
+    @Override
+    public String getName() {
+        return this.name;
+    }
+
+    private void setName(String name) {
+        if (name==null||name.trim().isEmpty()){
+            throw new IllegalArgumentException(ExceptionMessages.INVALID_NAME);
+        }else {
+            this.name = name;
+        }
+    }
+
+    @Override
+    public double getPortion() {
+        return this.portion;
+    }
+
+    private void setPortion(double portion) {
+        if (portion<=0){
+            throw new IllegalArgumentException(ExceptionMessages.INVALID_PORTION);
+        }else {
+            this.portion = portion;
+        }
+    }
+
+    @Override
+    public double getPrice() {
+        return this.price;
+    }
+
+    private void setPrice(double price) {
+        if (price<=0){
+            throw new IllegalArgumentException(ExceptionMessages.INVALID_PRICE);
+        }else {
+            this.price = price;
+        }
+    }
+}
