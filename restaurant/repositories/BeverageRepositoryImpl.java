8356a5cba94a9de29fdbc96e57d2088c3a7abfb5
OliaVladova <ovladova1@gmail.com>
2022-06-12 23:29:15 +0300

    Add files via upload

---
+++ restaurant/repositories/BeverageRepositoryImpl.java
@@ -0,0 +1,39 @@
+package restaurant.repositories;
+
+import restaurant.entities.drinks.interfaces.Beverages;
+import restaurant.repositories.interfaces.BeverageRepository;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class BeverageRepositoryImpl implements BeverageRepository<Beverages> {
+    private Map<String, Beverages> entities;
+
+    public BeverageRepositoryImpl() {
+        this.entities = new LinkedHashMap<>();
+    }
+
+    @Override
+    public Beverages beverageByName(String drinkName, String drinkBrand) {
+        return this.entities
+                .values()
+                .stream()
+                .filter(beverage -> beverage.getName().equals(drinkName) &&
+                        beverage.getBrand().equals(drinkBrand))
+                .findFirst()
+                .orElse(null);
+    }
+
+
+    @Override
+    public Collection<Beverages> getAllEntities() {
+        return Collections.unmodifiableCollection(this.entities.values());
+    }
+
+    @Override
+    public void add(Beverages entity) {
+        this.entities.put(entity.getName(), entity);
+    }
+}
